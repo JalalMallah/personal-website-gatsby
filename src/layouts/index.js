1eeba67a9eda61258c9a676466e5ff27bb42fac2
Jalal Mallah <jalal_mallah@live.com>
2021-04-18 15:31:21 +0200

    build: install font awesome and create basic structure of the header

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Header from 'components/Header';
 
 export default function Layout({ children }) {
   return (
     <>
-      <header>header</header>
+      <Header />
       <main>{children}</main>
     </>
   );
