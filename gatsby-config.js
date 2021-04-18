cd485d2cd61037ecb3aec7f25b49e1732019b311
Jalal Mallah <jalal_mallah@live.com>
2021-04-18 14:22:52 +0200

    chore: prepare basic structure of the project and delete unnecessary files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Jalal Mallah - Web Developer`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `Jalal Mallah`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -33,4 +33,4 @@ module.exports = {
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
   ],
-}
+};
