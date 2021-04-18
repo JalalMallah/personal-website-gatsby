b4615da4d198119edf39bc0a0537cec3be17efda
Jalal Mallah <jalal_mallah@live.com>
2021-04-18 21:02:00 +0200

    style: style the index page

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from 'components/Header/Header';
 
+import * as styles from 'styles/layout.module.scss';
+
 export default function Layout({ children }) {
   return (
     <>
       <Header />
-      <main>{children}</main>
+      <main className={styles.main}>{children}</main>
     </>
   );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,23 @@
 import * as React from 'react';
+import { Link } from 'gatsby';
+
+import heroImage from '../images/desk.svg';
+
+import * as styles from 'styles/indexPage.module.scss';
 
 const IndexPage = () => (
   <>
-    <h1>My mission is to create your dream website!</h1>
-    <img src="../images/desk-illustration.svg" alt="Hero illustration" />
-    <p>
+    <h1 className={styles.title}>
+      My mission is to create your dream website!
+    </h1>
+    <img src={heroImage} className={styles.hero} alt="hero desk illustration" />
+    <p className={styles.paragraph}>
       My name is Jalal Mallah and I’m a Front-End Web Developer. Let’s get to
       know each other!
     </p>
-    <button>my projects</button>
+    <Link to="/portfolio" className={styles.myProjectsButton}>
+      My Projects
+    </Link>
   </>
 );
 
