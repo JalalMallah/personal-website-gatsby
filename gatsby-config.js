1514db12a901d962adddcdf04b7850ab7a22d379
Jalal Mallah <jalal_mallah@live.com>
2021-04-18 14:58:23 +0200

    style: add global styles and set the baseURL in jsconfig.json to src by installing gatsby-plugin-resolve-src

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,6 +33,7 @@ module.exports = {
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
     `gatsby-plugin-layout`,
+    'gatsby-plugin-resolve-src',
     {
       resolve: `gatsby-plugin-sass`,
       options: {
