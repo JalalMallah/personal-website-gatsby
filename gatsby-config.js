bee8313a116047bc01352cfdaa7beab3150e26c4
Jalal Mallah <jalal_mallah@live.com>
2021-04-29 21:45:42 +0200

    feat: implement react helmet for including site metadata to the index.html page

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: `Jalal Mallah - Web Developer`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    description: `The personal website and portfolio of Jalal Mallah, a Front-End Web Developer`,
+    keywords: 'react, javascript, web development, website',
     author: `Jalal Mallah`,
   },
   plugins: [
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
+
 import Header from 'components/Header/Header';
 import Footer from 'components/Footer/Footer';
 
@@ -9,6 +11,18 @@ import * as styles from 'styles/layout.module.scss';
 export default function Layout({ children }) {
   return (
     <div className={styles.site}>
+      <Helmet defer={false}>
+        <title>Jalal Mallah - Web Developer</title>
+        <meta
+          name="description"
+          content="The personal website and portfolio of Jalal Mallah, a Front-End Web Developer"
+        />
+        <meta
+          name="keywords"
+          content="react, javascript, web development, website"
+        />
+        <meta name="author" content="Jalal Mallah" />
+      </Helmet>
       <Header />
       <main className={styles.main}>{children}</main>
       <Footer />
